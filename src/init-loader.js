8fcbbc73ee563b7f7a29493cad563edef14f8d2b
Kevin Mamaqi Kapllani <108928180+kevinmamaqi-90poe@users.noreply.github.com>
2023-08-18 15:52:36 +0200

    update

--- src/init-loader.js
+++ src/init-loader.js
@@ -1,18 +1,18 @@
-const { getOptions } = require('loader-utils')
+const { getOptions } = require("loader-utils");
 
 module.exports = function loader(source) {
-  const options = getOptions(this)
+  const options = getOptions(this);
 
   options.plugins.forEach((plugin) => {
-    source += `var ${plugin} = require('dayjs/plugin/${plugin}');`
-  })
+    source += `var ${plugin} = require('dayjs/plugin/${plugin}');`;
+  });
 
   options.plugins.forEach((plugin) => {
-    source += `dayjs.extend(${plugin});`
-  })
-  
+    source += `dayjs.extend(${plugin});`;
+  });
+
   // special plugin
-  source += `var antdPlugin = require('antd-dayjs-webpack-plugin/src/antd-plugin');dayjs.extend(antdPlugin);`
-  
-  return source
-}
+  source += `var antdPlugin = require('@90poe/antd-dayjs-webpack-plugin/src/antd-plugin');dayjs.extend(antdPlugin);`;
+
+  return source;
+};
